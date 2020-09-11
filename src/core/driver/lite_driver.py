4c52c3c9ecbaba1b93de41e233551746175ac517
Moonlight Han <hanweibin1@huawei.com>
2020-09-11 10:16:19 +0800

    update src/core/driver/lite_driver.py. Delete Chinese comments.

--- src/core/driver/lite_driver.py
+++ src/core/driver/lite_driver.py
@@ -55,7 +55,7 @@ class LiteUnitTest(IDriver):
         1. check serial protocol
         2. login device
         3. NFS is available
-        :param config:包含serial_protocol，serial_device，nfs_operation
+        :param config: serial device
         :return:
         """
         cls.log.error("Lite driver check config:{}".format(config))
