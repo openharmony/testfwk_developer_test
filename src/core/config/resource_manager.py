b09016bf67c44def4c2c76d28911506c7f550fda
Teacher_Dong <dongwang.wangdong@huawei.com>
2024-03-27 18:02:32 +0800

    Signed-off-by:Teacher_Dong<dongwang.wangdong@huawei.com>

--- src/core/config/resource_manager.py
+++ src/core/config/resource_manager.py
@@ -97,7 +97,7 @@ class ResourceManager(object):
             if os.path.exists(file_path):
                 tree = ElementTree.parse(file_path)
                 root = tree.getroot()
-                targets = root.getiterator("target")
+                targets = root.iter("target")
                 for target in targets:
                     curr_dic = target.attrib
                     if curr_dic.get("name") == targe_tname or \
